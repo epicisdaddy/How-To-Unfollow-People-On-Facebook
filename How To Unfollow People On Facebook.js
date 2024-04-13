a55c89a80fd59c1699b2012adfb102dcbd56e555
EpicIsDaddy <75577308+epicisdaddy@users.noreply.github.com>
2024-04-13 15:38:14 +0600

    Add files via upload

---
+++ How To Unfollow People On Facebook.js
@@ -0,0 +1,32 @@
+let intervalId = setInterval(function () {
+let manageButtons = document.querySelectorAll('div[aria-label="Manage"]');
+let manageButton = manageButtons[0];
+if (manageButton) {
+
+    setTimeout(() => {
+
+        manageButton.click();
+    }, 800);
+    var unfollowButtons = null;
+    let unfollowInterval = setInterval(function () {
+
+        setTimeout(() => {
+
+            unfollowButtons = document.querySelector('div[role="menuitem"]');
+            if(unfollowButtons){
+                unfollowButtons.click();
+                window.scrollBy(0, 60);
+            }
+        }, 700);
+        clearInterval(unfollowInterval);
+    }, 700);
+    setTimeout(() => {
+
+        manageButton.remove();
+        if(unfollowButtons){
+            unfollowButtons.click();
+        }
+    }, 1000);
+} else {
+}
+}, 700);
